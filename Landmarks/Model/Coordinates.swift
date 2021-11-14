614938e87c67388f18b7f24bd4aa6e3f2e365708
farukmeral25 <farukmeral25@gmail.com>
2021-11-14 13:53:00 +0300

    - Models

---
+++ Landmarks/Model/Coordinates.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinates.swift
+//  Landmarks
+//
+//  Created by Ömer Faruk Meral on 14.11.2021.
+//
+
+import Foundation
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
+
