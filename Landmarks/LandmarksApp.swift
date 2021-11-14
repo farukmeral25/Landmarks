57f08b1bef2f63fb6b07e966c365daeff076ab37
farukmeral25 <farukmeral25@gmail.com>
2021-11-14 13:33:59 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ömer Faruk Meral on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
